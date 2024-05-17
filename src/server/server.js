d28957dcfb7dc664a0ec62b45061ecefe77a7221
erlanig <erlanig@students.undip.ac.id>
2024-05-17 22:21:19 +0700

    add get histories

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const Hapi = require("@hapi/hapi");
 const routes = require("../server/routes");
 const loadModel = require("../services/loadModel");
+const InputError = require("../exceptions/InputError");
 
 (async () => {
   const server = Hapi.server({
@@ -11,6 +12,12 @@ const loadModel = require("../services/loadModel");
     routes: {
       cors: {
         origin: ["*"],
+      },
+      payload: {
+        maxBytes: 1000000,
+        parse: true,
+        output: "data",
+        multipart: true,
       },
     },
   });
@@ -23,22 +30,38 @@ const loadModel = require("../services/loadModel");
   server.ext("onPreResponse", function (request, h) {
     const response = request.response;
 
-    // if (response instanceof InputError) {
-    //     const newResponse = h.response({
-    //         status: 'fail',
-    //         message: `${response.message} Silakan gunakan foto lain.`
-    //     })
-    //     newResponse.code(response.statusCode)
-    //     return newResponse;
-    // }
-
-    if (response.isBoom) {
+    if (response instanceof InputError) {
       const newResponse = h.response({
         status: "fail",
-        message: response.message,
+        message: "Terjadi kesalahan dalam melakukan prediksi. Silakan gunakan foto lain.",
       });
-      newResponse.code(response.output.statusCode);
+      newResponse.code(400);
       return newResponse;
+    }
+
+    if (response.isBoom) {
+      if (response.output.statusCode === 413) {
+        const newResponse = h.response({
+          status: "fail",
+          message: "Payload content length greater than maximum allowed: 1000000",
+        });
+        newResponse.code(413);
+        return newResponse;
+      } else if (response.output.statusCode === 400) {
+        const newResponse = h.response({
+          status: "fail",
+          message: "Terjadi kesalahan dalam melakukan prediksi",
+        });
+        newResponse.code(400);
+        return newResponse;
+      } else {
+        const newResponse = h.response({
+          status: "fail",
+          message: response.message,
+        });
+        newResponse.code(response.output.statusCode);
+        return newResponse;
+      }
     }
 
     return h.continue;
